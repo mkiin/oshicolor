bfde96e8ea42d02bb8b1559000509c7e84db75a6
mkiin <blckcaties@gmail.com>
2026-03-13 07:58:07 +0900

    0.03に修正

--- scripts/gen-palette-svg-sharp.ts
+++ scripts/gen-palette-svg-sharp.ts
@@ -11,7 +11,7 @@ const OPTIONS_BASE = {
     quality: 10,
     colorSpace: "rgb" as const,
     ignoreWhite: true,
-    minSaturation: 0.05,
+    minSaturation: 0.03,
 };
 
 const OPTIONS = { ...OPTIONS_BASE, colorCount: 16 };
